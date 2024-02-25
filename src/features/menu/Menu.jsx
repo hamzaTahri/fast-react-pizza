f3a8dd413815e13234ff3b933144bad810e01ab0
Hamza TAHRI <werty.hamza@gmail.com>
2024-02-25 14:12:35 +0100

    project setup + data fetching

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,14 @@
+import {getMenu} from "../../services/apiRestaurant.js";
+import {useLoaderData} from "react-router-dom";
+import MenuItem from "./MenuItem.jsx";
+
+function Menu() {
+    const menu = useLoaderData();
+    return <ul>{menu.map((pizza) => (<MenuItem pizza={pizza} key={pizza.id}/>))}</ul>;
+}
+
+export async function loader() {
+    return await getMenu();
+}
+
+export default Menu;
---
+++ src/features/order/Order.jsx
@@ -0,0 +1,56 @@
+// Test ID: IIDSAT
+
+import {calcMinutesLeft, formatCurrency, formatDate,} from "../../utils/helpers";
+import {getOrder} from "../../services/apiRestaurant.js";
+import {useLoaderData} from "react-router-dom";
+
+function Order() {
+    // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
+    const order = useLoaderData();
+    const {
+        // eslint-disable-next-line no-unused-vars
+        id,
+        status,
+        priority,
+        priorityPrice,
+        orderPrice,
+        estimatedDelivery,
+        // eslint-disable-next-line no-unused-vars
+        cart,
+    } = order;
+    const deliveryIn = calcMinutesLeft(estimatedDelivery);
+
+    return (
+        <div>
+            <div>
+                <h2>Status</h2>
+
+                <div>
+                    {priority && <span>Priority</span>}
+                    <span>{status} order</span>
+                </div>
+            </div>
+
+            <div>
+                <p>
+                    {deliveryIn >= 0
+                        ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
+                        : "Order should have arrived"}
+                </p>
+                <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
+            </div>
+
+            <div>
+                <p>Price pizza: {formatCurrency(orderPrice)}</p>
+                {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
+                <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+            </div>
+        </div>
+    );
+}
+
+export async function loader({params}) {
+    return await getOrder(params.orderId);
+}
+
+export default Order;
