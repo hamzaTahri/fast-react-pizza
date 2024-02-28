b5ce0dc908c5baaa8dacc10167468da3df053be7
Hamza TAHRI <werty.hamza@gmail.com>
2024-02-28 16:16:01 +0100

    finished tailwind css integration

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,24 +1,53 @@
-// eslint-disable-next-line react/prop-types
-import {formatCurrency} from "../../utils/helpers.js";
+import Button from '../../ui/Button';
+import {formatCurrency} from '../../utils/helpers';
+import {useDispatch} from "react-redux";
+import {addItem} from "../cart/cartSlice.js";
 
 // eslint-disable-next-line react/prop-types
-function MenuItem({ pizza }) {
-    // eslint-disable-next-line no-unused-vars,react/prop-types
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+function MenuItem({pizza}) {
+    // eslint-disable-next-line react/prop-types,no-unused-vars
+    const {id, name, unitPrice, ingredients, soldOut, imageUrl} = pizza;
+    const dispatch = useDispatch();
 
-  return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-          {/* eslint-disable-next-line react/prop-types */}
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
-      </div>
-    </li>
-  );
+    function handleAddToCart() {
+        const newPizza = {
+            pizzaId: id,
+            name,
+            quantity: 1,
+            unitPrice,
+            totalPrice: unitPrice * 1,
+        }
+        console.log(id)
+        dispatch(addItem(newPizza))
+    }
+
+    return (
+        <li className="flex gap-4 py-2">
+            <img
+                src={imageUrl}
+                alt={name}
+                className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+            />
+            <div className="flex grow flex-col pt-0.5">
+                <p className="font-medium">{name}</p>
+                <p className="text-sm capitalize italic text-stone-500">
+                    {/* eslint-disable-next-line react/prop-types */}
+                    {ingredients.join(', ')}
+                </p>
+                <div className="mt-auto flex items-center justify-between">
+                    {!soldOut ? (
+                        <p className="text-sm">{formatCurrency(unitPrice)}</p>
+                    ) : (
+                        <p className="text-sm font-medium uppercase text-stone-500">
+                            Sold out
+                        </p>
+                    )}
+                    {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>
+                    }
+                </div>
+            </div>
+        </li>
+    );
 }
 
 export default MenuItem;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,26 @@
+import CreateUser from '../features/user/CreateUser';
+import {useSelector} from "react-redux";
+import Button from "./Button.jsx";
+
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+    const username = useSelector(state => state.user.username);
+    return (
+        <div className="my-10 px-4 text-center sm:my-16">
+            <h1 className="mb-8  text-xl font-semibold md:text-3xl">
+                The best pizza.
+                <br/>
+                <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
+            </h1>
+
+            {
+                username === '' ? <CreateUser/> :
+                    <Button type='primary' to='/menu'>Continue Ordering, {username}</Button>
+            }
+
+        </div>
+    );
 }
 
 export default Home;
